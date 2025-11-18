75cc272026b9a1ab416eb26cac861cc312415df4
Lucas Ângelo <lucasangeloss27@gmail.com>
2025-11-18 20:03:36 -0300

    Adicionar status no Banco de Dados

--- tarefas.sql
+++ tarefas.sql
@@ -4,5 +4,6 @@ CR"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" -u root -p
 CREATE TABLE tarefas (
     id INT PRIMARY KEY AUTO_INCREMENT,
     resposta VARCHAR(75) NOT NULL,
-    dificuldade VARCHAR(20) NOT NULL
+    dificuldade VARCHAR(20) NOT NULL,
+    status VARCHAR(20) DEFAULT 'pendente'
 );

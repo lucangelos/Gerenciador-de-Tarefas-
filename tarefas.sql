47c6cfd6993419206ef9a21fdf15a0566d93b068
Lucas Ângelo <lucasangeloss27@gmail.com>
2025-11-25 16:59:39 -0300

    Modificações no banco de dados
    Removed MySQL command line connection and added user creation with privileges.

--- tarefas.sql
+++ tarefas.sql
@@ -1,9 +1,11 @@
-CR"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" -u root -p
- gerenciador_de_tarefas;
-
 CREATE TABLE tarefas (
     id INT PRIMARY KEY AUTO_INCREMENT,
     resposta VARCHAR(75) NOT NULL,
     dificuldade VARCHAR(20) NOT NULL,
     status VARCHAR(20) DEFAULT 'pendente'
 );
+
+CREATE USER 'appuser'@'%' IDENTIFIED BY 'senha123';
+GRANT ALL PRIVILEGES ON gerenciador_de_tarefas.* TO 'appuser'@'%';
+FLUSH PRIVILEGES;
+
